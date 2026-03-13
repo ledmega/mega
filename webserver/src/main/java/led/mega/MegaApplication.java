a43e8fe9cb2683c3f11dc8c8065385a574ff7dce
ledme <ledme@example.com>
2026-03-13 14:05:54 +0900

    [FIX] Enable scheduling in MegaApplication to provide TaskScheduler bean

--- webserver/src/main/java/led/mega/MegaApplication.java
+++ webserver/src/main/java/led/mega/MegaApplication.java
@@ -1,9 +1,10 @@
 package led.mega;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MegaApplication {
 
 	@jakarta.annotation.PostConstruct
