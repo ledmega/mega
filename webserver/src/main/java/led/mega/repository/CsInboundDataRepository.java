3c4b0b9a520c734b3f181536c4f218ad733ae40c
ledme <ledme@example.com>
2026-03-18 13:25:58 +0900

    fix(cs): add missing Query import to repository

--- webserver/src/main/java/led/mega/repository/CsInboundDataRepository.java
+++ webserver/src/main/java/led/mega/repository/CsInboundDataRepository.java
@@ -1,6 +1,7 @@
 package led.mega.repository;
 
 import led.mega.entity.CsInboundData;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
